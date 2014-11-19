c378b52bfba8388fdaf706241dbd7df28d78675a
VACSA <vacsaman@gmail.com>
2014-11-20 02:05:16 +0400

    node fs write/read/readAsync

--- NodeTest/t_1/main.js
+++ NodeTest/t_1/main.js
@@ -1,4 +1,17 @@
-/**
- * Created by 1 on 20.11.2014.
- */
 var fs = require('fs');
+//console.log(fs);
+fs.writeFile('testFile.txt', "test tedfgdfgdfgdfxt", function (err) {
+    if (err) throw error;
+    // after write
+})
+
+var fileName = "testFile.txt";
+//Sync blocked methods
+fs.exists(fileName, function (exists) {
+    if (exists) {
+        var contents = fs.readFileSync(fileName, '', function (err) {
+            if (err)  throw err;
+        });
+        console.log("testBuffer: ", contents);
+    }
+});
